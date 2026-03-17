0db8f51b27ac851f0e40bc666b2164d567a91c57
Salih İlgaz <ilgazmuhammedsalih1@gmail.com>
2026-03-17 05:10:14 +0300

    fix: Router basename silindi siyah ekran cözüldü

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/HUB/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
